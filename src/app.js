0ff9de81cc3dcf91f05727685354680cfa74989d
Folkenciyo <folkencillo@gmail.com>
2021-10-14 11:27:14 +0000

    exerciseFinished

--- src/app.js
+++ src/app.js
@@ -6,6 +6,42 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#domain").innerHTML = generateDomain();
+  document.querySelector("#domain2").innerHTML = generateDomain();
+  console.log("e");
+};
+
+let generateDomain = () => {
+  let domineStart = ["http://www."];
+  let domineFirst = ["caza", "tiembla", "gico", "muerde"];
+  let domineSecond = [
+    "royo",
+    "fiesta",
+    "nori",
+    "lepo",
+    "tuki",
+    "mambo",
+    "troncho"
+  ];
+  let domineThird = ["company", "shop", "market", "sellers"];
+  let domineEnd = [".com", ".es", ".net"];
+
+  let startIndex = generateRandomNumber(domineStart);
+  let firstIndex = generateRandomNumber(domineFirst);
+  let secondIndex = generateRandomNumber(domineSecond);
+  let thirdIndex = generateRandomNumber(domineThird);
+  let endIndex = generateRandomNumber(domineEnd);
+
+  function generateRandomNumber(dominePosition) {
+    let randomNumber = Math.floor(Math.random() * dominePosition.length);
+    return randomNumber;
+  }
+
+  return (
+    domineStart[startIndex] +
+    domineFirst[firstIndex] +
+    domineSecond[secondIndex] +
+    domineThird[thirdIndex] +
+    domineEnd[endIndex]
+  );
 };
